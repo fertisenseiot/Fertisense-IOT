832177afca10a2a1efa4cc4316bdded23e2a8e2d
fertisenseiot <fertisenseiot@gmail.com>
2026-03-25 18:10:51 +0530

    Update user_dashboard.js

--- iot_api/static/js/user_dashboard.js
+++ iot_api/static/js/user_dashboard.js
@@ -1954,7 +1954,7 @@ tooltip: {
       y: {
         beginAtZero: true,
         // grace: '10%',
-        //min:0,
+        min:-250,
         grid: { color: "rgba(0,0,0,0.06)" },
 
         ticks: {
